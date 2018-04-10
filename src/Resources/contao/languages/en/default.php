bd08fc3614f03e3187e8dca6e4638b282b8673aa
Christopher Mühl <christopher@dieschittigs.de>
2018-04-10 11:35:59 +0200

    Add english localization

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['ERR']['recaptcha'] = "reCAPTCHA could not verify that you're not a robot. Please try again.";
---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['recaptchaType'][0] = "reCAPTCHA-Type";
+$GLOBALS['TL_LANG']['tl_settings']['recaptchaPublicKey'][0] = "Site key";
+$GLOBALS['TL_LANG']['tl_settings']['recaptchaPublicKey'][1] = "The key that's getting served to your users.";
+$GLOBALS['TL_LANG']['tl_settings']['recaptchaPrivateKey'][0] = "Secret key";
+$GLOBALS['TL_LANG']['tl_settings']['recaptchaPrivateKey'][1] = "They key that's used for communication between Contao and Google.";
+
+$GLOBALS['TL_LANG']['tl_settings']['recaptcha_legend'] = "Security: Google reCAPTCHA";
