2095f8b06fa1126b67837d0df057baa86a1f4f22
Marko Cupic <m.cupic@gmx.ch>
2020-07-16 23:04:15 +0200

    Weiterer workaround um den Bug in den Contao Backendeinstellungen zu fixen
    Wäre super, wenn der commit getagt würde. Danke.

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,48 +1,50 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{recaptcha_legend},recaptchaType;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+// Extend the default palette
+Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+    ->addLegend('recaptcha_legend', 'files_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
+    ->addField(['recaptchaType', 'recaptchaPublicKey', 'recaptchaPrivateKey'], 'recaptcha_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_settings');
 
-$GLOBALS['TL_DCA']['tl_settings']['config']['onload_callback'][] = function() {
+if (Contao\Config::get('recaptchaType') === 'recaptcha3')
+{
+    // Add additional field if Contao\Config::get('recaptchaType') === 'recaptcha3'
+    Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+        ->addField(['recaptcha3GlobalThreshold'], 'recaptchaType', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
+        ->applyToPalette('default', 'tl_settings');
+}
 
-    switch( \Config::get('recaptchaType') ) {
 
-        case 'recaptcha3':
-            $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('recaptchaType','recaptchaType,recaptcha3GlobalThreshold,recaptchaPublicKey,recaptchaPrivateKey', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
-            break;
-        default:
-            $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('recaptchaType','recaptchaType,recaptchaPublicKey,recaptchaPrivateKey', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
-            break;
-    }
-};
-
+/**
+ * Fields
+ */
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
-    'recaptchaType' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaType'],
-        'inputType'         => 'select',
-        'options_callback'  => function ()
-        {
+    'recaptchaType'             => [
+        'label'            => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaType'],
+        'inputType'        => 'select',
+        'options_callback' => function () {
             return [
-                'invisible' => 'reCAPTCHA v2: Invisible',
+                'invisible'  => 'reCAPTCHA v2: Invisible',
                 'recaptcha2' => 'reCAPTCHA v2: Checkbox',
                 'recaptcha3' => 'reCAPTCHA v3',
             ];
         },
-        'eval'              => ['tl_class' => 'w50', 'chosen' => true, 'submitOnChange' => true],
+        'eval'             => ['tl_class' => 'w50', 'chosen' => true, 'submitOnChange' => true],
     ],
-    'recaptchaPublicKey' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaPublicKey'],
-        'inputType'         => 'text',
-        'eval'              => ['tl_class' => 'w50 clr'],
+    'recaptchaPublicKey'        => [
+        'label'     => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaPublicKey'],
+        'inputType' => 'text',
+        'eval'      => ['tl_class' => 'w50 clr'],
     ],
-    'recaptchaPrivateKey' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaPrivateKey'],
-        'inputType'         => 'text',
-        'eval'              => ['tl_class' => 'w50'],
+    'recaptchaPrivateKey'       => [
+        'label'     => &$GLOBALS['TL_LANG']['tl_settings']['recaptchaPrivateKey'],
+        'inputType' => 'text',
+        'eval'      => ['tl_class' => 'w50'],
     ],
     'recaptcha3GlobalThreshold' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['recaptcha3GlobalThreshold'],
-        'inputType'         => 'text',
-        'default'           => '0.5',
-        'eval'              => ['tl_class' => 'w50'],
+        'label'     => &$GLOBALS['TL_LANG']['tl_settings']['recaptcha3GlobalThreshold'],
+        'inputType' => 'text',
+        'default'   => '0.5',
+        'eval'      => ['tl_class' => 'w50'],
     ],
 ];
